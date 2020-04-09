62d66e08313302983c5ffdae3b5fa4c04a96e54e
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-09 15:31:18 +0000

    Done.

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,8 +1,10 @@
 def find_max_value(array)
   count = 0
-  maximun_value = 0
-  while count <= array.length do
-    count = maximun_value
+  maximun_value = -1
+  while count < array.length do
+    if maximun_value < array[count]
+      maximun_value = array [count]
+    end
     count += 1
   end
   maximun_value
